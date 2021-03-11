ef9dd8bea4e6f5761555f0bfd7afff0310796107
Weliton de Almeida <79901750+welitonalmeida@users.noreply.github.com>
2021-03-11 01:05:36 -0300

    Upload dos arquivos
    Upload dos arquivos da calculadora agroclimática desenvolvida em C#

---
+++ MainForm.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Calculadora_Agroclimática
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label4 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label4
+			// 
+			this.label4.BackColor = System.Drawing.Color.Transparent;
+			this.label4.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(12, 9);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(389, 35);
+			this.label4.TabIndex = 0;
+			this.label4.Text = "Calculadora Agroclimática";
+			this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label1
+			// 
+			this.label1.BackColor = System.Drawing.Color.Transparent;
+			this.label1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(12, 92);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(194, 21);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Dia de ordem do ano:";
+			// 
+			// label2
+			// 
+			this.label2.BackColor = System.Drawing.Color.Transparent;
+			this.label2.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(12, 153);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(194, 21);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Latitude em °:";
+			// 
+			// label3
+			// 
+			this.label3.BackColor = System.Drawing.Color.White;
+			this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.label3.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(12, 258);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(389, 81);
+			this.label3.TabIndex = 3;
+			this.label3.Text = "Resultados:\r\n";
+			this.label3.Click += new System.EventHandler(this.Label3Click);
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.button1.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.ForeColor = System.Drawing.Color.White;
+			this.button1.Location = new System.Drawing.Point(140, 202);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(137, 41);
+			this.button1.TabIndex = 4;
+			this.button1.Text = "Calcular";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(188, 93);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(99, 20);
+			this.textBox1.TabIndex = 5;
+			this.textBox1.TextChanged += new System.EventHandler(this.TextBox1TextChanged);
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(188, 155);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(99, 20);
+			this.textBox2.TabIndex = 6;
+			this.textBox2.TextChanged += new System.EventHandler(this.TextBox2TextChanged);
+			// 
+			// label5
+			// 
+			this.label5.BackColor = System.Drawing.Color.Transparent;
+			this.label5.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+			this.label5.Location = new System.Drawing.Point(293, 92);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(108, 21);
+			this.label5.TabIndex = 7;
+			this.label5.Text = "Ex: 64 para 05/03";
+			this.label5.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+			// 
+			// label6
+			// 
+			this.label6.BackColor = System.Drawing.Color.Transparent;
+			this.label6.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label6.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+			this.label6.Location = new System.Drawing.Point(293, 155);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(108, 21);
+			this.label6.TabIndex = 8;
+			this.label6.Text = "Ex: -29,3456";
+			this.label6.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(418, 348);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.label4);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Calculadora Agroclimática";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+	}
+}
---
+++ MainForm.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Calculadora_Agroclimática
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void TextBox1TextChanged(object sender, EventArgs e)
+		{
+			//Dia de ordem do ano
+			
+		}
+		void TextBox2TextChanged(object sender, EventArgs e)
+		{
+			//Latitude
+			
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			//Botão 'Calcular'
+			
+			try
+			{
+				label3.ResetText();
+				
+				//Cálculo da declinação solar:
+				//Equação: declinação=23,45*Sen((360/365)*(284+doa))      "doa = Dia de ordem do ano"
+				
+				int doa = Convert.ToInt16(textBox1.Text);
+				int num1 = 284 + doa;
+				float num2 = 0.986301369f;
+				float num3 = (num1 * num2);
+				double num4 = Convert.ToDouble(num3);
+				double seno = Math.Sin((Math.PI) * (num4 / 180.0));
+				double declinação = seno * 23.45;
+				
+				//Cálculo das horas/luz para o dia:
+				//Equação: horas=(2/15)*Acos(-Tan(latitude)*Tan(declinação))
+				
+				double latitude = Convert.ToDouble(textBox2.Text);
+				double num5 = (-Math.Tan((Math.PI) * (latitude / 180.0)));
+				double num6 = (Math.Tan((Math.PI) * (declinação / 180.0)));
+				double num7 = (num5 * num6);
+				double num8 = (Math.Acos(num7) * (180 / Math.PI));
+				float num9 = 0.1333333f;
+				double num10 = num9;
+				double horas = (num8 * num9);
+				
+				//Escreve na label3 a declinação:
+				label3.Text=label3.Text+"Resultados: " + "\n" + "A declinação solar para o " + doa + "° dia é: " + Math.Round(declinação, 4) + "°" + "\n";
+				
+				//Escreve na label3 o números de horas de luz:
+				label3.Text=label3.Text+ "A duração astronômica do " + doa + "° dia é: " + Math.Round(horas, 2) + " horas.";
+				
+			}
+			catch(Exception){
+				MessageBox.Show("ERRO");
+			}
+		}
+		void Label3Click(object sender, EventArgs e)
+		{
+			//Caixa de saída dos resultados
+		}
+	}
+}
